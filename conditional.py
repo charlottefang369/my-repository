0eb466f9d5bddf454de8108a42bfdb5c75b6a16c
charlottefang369 <charlottefang369@gmail.com>
2026-03-06 11:34:45 +0000

    delete conditional py. added py.py

--- conditional.py
+++
@@ -1,5 +0,0 @@
-value = int(input('Enter a number: '))
-
-if value == 3:
-    print('value is 3')
-    
---
+++ py.py
@@ -0,0 +1,11 @@
+text = 'aaabaacccabxyzabccba'
+
+text.startswith('aaa')
+text.startswith('aaa', 4, 8)
+text.startswith('aaa', 5)
+text.startswith(('aaa', 'abf', 'fhe'))
+
+text.endswith('aaa')
+text.endswith('aaa', 4, 8)
+text.endswith('aaa', 5)
+
